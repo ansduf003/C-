65d2599716893cb87fbe12755a7b38a6c484c6c3
Moon Yeol <46176934+ansduf003@users.noreply.github.com>
2023-05-26 09:57:11 +0900

    Create db_MS.cs

---
+++ db_MS.cs
@@ -0,0 +1,28 @@
+        SqlConnection dbConection()
+        {
+            string strDataBase = "TEST_DB";          //Database
+            string strIP = "127.0.0.1";         //Ip
+            string strPort = "1433";            //Port
+            string strID = "nodedb";              //ID
+            string strPW = "1234567890";				//PW
+            
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                // DB 접속 정보
+                string constring = "server=" + strIP +
+                        "," + strPort +
+                        ";database=" + strDataBase +
+                        ";uid=" + strID +
+                        ";pwd=" + strPW;
+                // 접속정보를 적용
+                sqlConnection.ConnectionString = constring;
+                // DB연결
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception error)
+            {
+                return null;
+            }
+        }
